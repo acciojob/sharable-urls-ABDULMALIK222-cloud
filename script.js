11bf552320fc3f06c2cac1f009992c278861a731
Abdul Malik <128873080+ABDULMALIK222-cloud@users.noreply.github.com>
2025-03-01 09:38:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// your code here
-// your code here
-let newUrl = `https://localhost:8080/?name=${name}&year=${year}`;
-document.getElementById('myForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-	document.getElementById('url').innerText = newUrl;
-
-  // Rest of the code goes here
+const app = require("./index.js");
+app.listen(3000, () => {
+  console.log('server started');
 });
